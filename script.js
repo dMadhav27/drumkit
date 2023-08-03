1d562db70f89ed54219c4e1acb527b65bab9adf7
dMadhav27 <120191817+dMadhav27@users.noreply.github.com>
2023-08-03 15:45:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+
+var noofdrumbuttons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<noofdrumbuttons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonInnerHTML = this.innerHTML;
+        makesound(buttonInnerHTML);
+        buttonanimation(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keypress", function(event){
+
+    makesound(event.key);
+    buttonanimation(event.key)
+});
+
+function makesound(key) {
+    switch (key) {
+        case "w":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+
+            break;
+    
+
+            case "a":
+                var crash = new Audio("./sounds/crash.mp3");
+                crash.play();
+                
+                break;
+
+
+                case "s":
+                    var snare = new Audio("./sounds/snare.mp3");
+                    snare.play();
+                    
+                    break;
+
+
+                    case "d":
+                        var tom1 = new Audio("./sounds/tom-1.mp3");
+                        tom1.play();
+                        
+                        break;
+
+                        case "j":
+                        var tom2 = new Audio("./sounds/tom-2.mp3");
+                        tom2.play();
+                        
+                        break;
+
+                        case "k":
+                        var tom3 = new Audio("./sounds/tom-3.mp3");
+                        tom3.play();
+                        
+                        break;
+
+                        case "l":
+                        var tom4 = new Audio("./sounds/tom-4.mp3");
+                        tom4.play();
+                        
+                        break;
+
+        default:
+            break;
+    }
+}
+
+
+function buttonanimation(currentkey) {
+
+   var activebutton=  document.querySelector("." + currentkey);
+
+  activebutton.classList.add("pressed");
+
+
+  setTimeout(function(){
+    activebutton.classList.remove("pressed");
+  }, 100)
+}
